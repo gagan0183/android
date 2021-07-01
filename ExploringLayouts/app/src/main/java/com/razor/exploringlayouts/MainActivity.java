fc0e25a226bbf6dbb46fde93d4a7f4b29b62d60b
gagan0183 <g.deepsingh1@gmail.com>
2021-07-02 00:40:44 +0530

    constraint and table layout methods

--- ExploringLayouts/app/src/main/java/com/razor/exploringlayouts/MainActivity.java
+++ ExploringLayouts/app/src/main/java/com/razor/exploringlayouts/MainActivity.java
@@ -3,6 +3,7 @@ package com.razor.exploringlayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+    }
+
+    public void setConstraintLayout(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setTableLayout(View view) {
+//        setContentView(R.layout.my_table_layout);
     }
 }
