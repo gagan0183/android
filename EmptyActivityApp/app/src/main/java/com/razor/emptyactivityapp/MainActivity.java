c120180d7c77ad726ab5d2ee60b777f56c94543e
gagan0183 <g.deepsingh1@gmail.com>
2021-07-02 00:22:56 +0530

    exploring layout project

--- EmptyActivityApp/app/src/main/java/com/razor/emptyactivityapp/MainActivity.java
+++ EmptyActivityApp/app/src/main/java/com/razor/emptyactivityapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.razor.emptyactivityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+    public void loadConstraintLayout(View view) {
         setContentView(R.layout.activity_main);
+    }
+
+    public void setTableLayout(View view) {
+//        setContentView(R.layout.my_table_layout);
     }
 }
